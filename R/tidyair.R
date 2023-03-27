9425cc6fa15f2d278107efe80eb8aa302737dc5d
umatter <umatter@protonmail.com>
2023-03-27 16:29:08 +0200

    clean up repository, improve robustness regarding dependencies

--- R/tidyair.R
+++ R/tidyair.R
@@ -11,9 +11,9 @@
 #' @return A data.frame containing the processed and tidied air data if the input is
 #'   a data.frame, or a character string with the file path of the generated CSV file if the input is a file.
 #' @author Ulrich Matter umatter@protonmail.com
-#' 
+#'
 #' @export
-#' 
+#'
 #' @examples
 #' \dontrun{
 #' # Create a data.frame with air data
@@ -28,54 +28,53 @@
 #' tidied_data <- tidyair(air_data)
 #' print(tidied_data)
 #' }
-#' 
+#'
 tidyair <- function(file) {
-  
+
   is_df <- is.data.frame(file)
   if (is_df) {
-    file <- df_to_csv(file)
+    file <- OpenAIR::df_to_csv(file)
   }
-  
+
   # import, process text
-  r_function <- read_text(file)
-  text <- 
-    r_function$text %>% 
+  r_function <- OpenAIR::read_text(file)
+  text <-
+    r_function$text %>%
     paste0(collapse = "\n")
-  
-  # Create user input 
-  input <- tidyair_input
+
+  # Create user input
   n_msgs <- nrow(tidyair_input)
-  input$content[n_msgs] <- 
-    sprintf(fmt = input$content[n_msgs], text)
-  
-  # Generate response output by chatting 
-  resp <- chat_completion(input)
-  total_tokens_used <- usage(resp)$total_tokens
+  tidyair_input$content[n_msgs] <-
+    sprintf(fmt = tidyair_input$content[n_msgs], text)
+
+  # Generate response output by chatting
+  resp <- OpenAIR::chat_completion(tidyair_input)
+  total_tokens_used <- OpenAIR::usage(resp)$total_tokens
   message("Total tokens used: ", total_tokens_used)
-  
+
   # extract output
-  output <- 
-    resp %>% 
-    messages_content()
-  
+  output <-
+    resp %>%
+    OpenAIR::messages_content()
+
   # process output
   filename <- unique(r_function$file)
-  
+
   if (filename == "character string") {
     # console/df input is returned as df
     output_df <- readr::read_csv(output)
     return(output_df)
-    
+
   } else {
     # file name for csv file
-    filename <- paste0(replace_file_extension(filename, ""), "-tidy.csv")
-    
+    filename <- paste0(OpenAIR::replace_file_extension(filename, ""), "-tidy.csv")
+
     # parse and write csv
     output_df <- readr::read_csv(output)
     readr::write_csv(output_df, file = filename)
     message("CSV-file generated: ", filename)
-    
+
     return(filename)
   }
-  
+
 }
---
+++ README.md
@@ -0,0 +1,27 @@
+
+# tidyair
+
+<!-- badges: start -->
+ [![Lifecycle: experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
+<!-- badges: end -->
+
+The goal of tidyair is to ...
+
+## Installation
+
+You can install the development version of tidyair from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("umatter/tidyair")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(tidyair)
+## basic example code
+```
+
